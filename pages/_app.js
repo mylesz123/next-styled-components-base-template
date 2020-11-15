13259de6c3aa2310e3a059b54b8e5a2f0e02f99c
Myles Young <myles.young@mailchimp.com>
2020-11-15 18:32:31 -0500

    breakout styles components and main files

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/globals.css';
+import '../styles/flaticon.css';
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import Head from 'next/head';
-import styled, { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
- h1 {
-   font-size: 3rem;
- }
-`;
-
-const Main = styled.div`
-  text-align: center;
-`;
+import { Main } from './styles';
+import { ResetCSS } from '../styles/reset';
 
 export default function Home() {
   return (
@@ -18,7 +9,7 @@ export default function Home() {
         <title>Home</title>
       </Head>
       <Main>
-        <GlobalStyle />
+        <ResetCSS />
         <h1>Yo!</h1>
       </Main>
     </>
