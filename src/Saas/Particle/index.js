437ede2497ebfd79666b35db1dad96b7828ef879
Myles Young <myles.young@mailchimp.com>
2020-12-02 19:07:24 -0500

    update particle

--- src/Saas/Particle/index.js
+++ src/Saas/Particle/index.js
@@ -1,84 +1,81 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 
-import Particle1 from '../../../assets/saas/particles/particle-1.png';
-import Particle2 from '../../../assets/saas/particles/particle-2.png';
-import Particle3 from '../../../assets/saas/particles/particle-3.png';
-import Particle4 from '../../../assets/saas/particles/particle-4.png';
-import Particle5 from '../../../assets/saas/particles/particle-5.png';
-import Particle6 from '../../../assets/saas/particles/particle-6.png';
+const particles = {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: '#ffffff' },
+    shape: {
+      type: 'circle',
+      stroke: { width: 0, color: '#000000' },
+      polygon: { nb_sides: 3 },
+      image: { src: 'img/github.svg', width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.6493818846505367,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 20.042650760819036,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 4.872463273808071,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 2000,
+      color: '#ffffff',
+      opacity: 0,
+      width: 18.15039575754227,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: { enable: true, mode: 'repulse' },
+      onclick: { enable: true, mode: 'push' },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+};
 
 const ParticlesComponent = () => (
   <>
-    <Particles
-      className="particle"
-      params={{
-        particles: {
-          number: {
-            value: 6,
-            density: { enable: true, value_area: 800 },
-          },
-
-          shape: {
-            type: ['images'],
-            images: [
-              {
-                src: `${Particle1}`,
-                width: 25,
-                height: 25,
-              },
-              {
-                src: `${Particle2}`,
-                width: 18,
-                height: 18,
-              },
-              {
-                src: `${Particle3}`,
-                width: 32,
-                height: 32,
-              },
-              {
-                src: `${Particle4}`,
-                width: 41,
-                height: 41,
-              },
-              {
-                src: `${Particle5}`,
-                width: 22,
-                height: 22,
-              },
-              {
-                src: `${Particle6}`,
-                width: 22,
-                height: 22,
-              },
-            ],
-          },
-          opacity: {
-            value: 0.17626369048095938,
-            random: true,
-            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-          },
-          size: {
-            value: 10,
-            random: false,
-          },
-          line_linked: {
-            enable: false,
-          },
-          move: {
-            enable: true,
-            speed: 1.5,
-            direction: 'none',
-            random: false,
-            straight: false,
-            bounce: true,
-            attract: { enable: true, rotateX: 100, rotateY: 400 },
-          },
-        },
-        retina_detect: true,
-      }}
-    />
+    <Particles className="particle" params={particles} />
   </>
 );
 export default ParticlesComponent;
