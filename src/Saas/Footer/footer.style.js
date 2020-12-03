fdc9455151e99073b63a920236e07ae3cb959ffd
Myles Young <myles.young@mailchimp.com>
2020-12-02 19:09:05 -0500

    add curve to footer

--- src/Saas/Footer/footer.style.js
+++ src/Saas/Footer/footer.style.js
@@ -1,29 +1,15 @@
 import styled from 'styled-components';
-import FooterImage from '../../../assets/saas/illustration.png';
+import { themeGet } from '@styled-system/theme-get';
+
+export const CurvIcon = styled.div`
+  svg {
+    fill: ${themeGet('colors.lightBB', '#f7f8fd')};
+  }
+`;
 
 const FooterWrapper = styled.footer`
   position: relative;
-  background-color: #f9fbfd;
   overflow: hidden;
-  background: linear-gradient(to bottom, #f7f8fd 0%, #fafbff 100%);
-  @media (min-width: 576px) {
-    padding-top: 130px;
-    &:before {
-      content: '';
-      position: absolute;
-      width: 104%;
-      padding-bottom: 104%;
-      border-top-right-radius: 11%;
-      top: 14%;
-      left: 0;
-      pointer-events: none;
-      background-color: #fff;
-      transform: rotate(-6deg);
-      @media (max-width: 767px) {
-        padding-bottom: 150%;
-      }
-    }
-  }
 
   .footer_container {
     background-repeat: no-repeat;
@@ -31,9 +17,6 @@ const FooterWrapper = styled.footer`
     padding-top: 80px;
     padding-bottom: 80px;
     position: relative;
-    @media (min-width: 576px) {
-      background-image: url(${FooterImage});
-    }
     @media (max-width: 990px) {
       padding-bottom: 20px;
     }
--- src/Saas/Footer/index.js
+++ src/Saas/Footer/index.js
@@ -1,42 +1,83 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import LogoImage from '../../../assets/saas/logo.png';
+import LogoImage from '../../../assets/rc/rc-logo.png';
 import { FooterData } from '../../data/Saas';
-import { Box, Container, Heading, Logo, Text } from '../../components';
+import {
+  Box,
+  Container,
+  Heading,
+  Logo,
+  Text,
+} from '../../components';
 
-import FooterWrapper, { List, ListItem } from './footer.style';
+import FooterWrapper, {
+  List,
+  ListItem,
+  CurvIcon,
+} from './footer.style';
 
-const Footer = ({ row, col, colOne, colTwo, titleStyle, logoStyle, textStyle }) => (
-  <FooterWrapper>
-    <Container className="footer_container">
-      <Box className="row" {...row}>
-        <Box {...colOne}>
-          <Logo href="#" logoSrc={LogoImage} title="Hosting" logoStyle={logoStyle} />
-          <Text content="hello@redq.io" {...textStyle} />
-          <Text content="+479-443-9334" {...textStyle} />
+const Curv = () => (
+  <CurvIcon>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fillOpacity="1"
+        d="M0,32L34.3,42.7C68.6,53,137,75,206,112C274.3,149,343,203,411,218.7C480,235,549,213,617,186.7C685.7,160,754,128,823,144C891.4,160,960,224,1029,256C1097.1,288,1166,288,1234,256C1302.9,224,1371,160,1406,128L1440,96L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"
+      />
+    </svg>
+  </CurvIcon>
+);
+
+const Footer = ({
+  row,
+  col,
+  colOne,
+  colTwo,
+  titleStyle,
+  logoStyle,
+  textStyle,
+}) => (
+  <>
+    <Curv />
+
+    <FooterWrapper>
+      <Container className="footer_container">
+        <Box className="row" {...row}>
+          <Box {...colOne}>
+            <Logo
+              href="#"
+              logoSrc={LogoImage}
+              title="Hosting"
+              logoStyle={logoStyle}
+            />
+            <Text content="hello@redq.io" {...textStyle} />
+            <Text content="+479-443-9334" {...textStyle} />
+          </Box>
+
+          <Box {...colTwo}>
+            {FooterData.map((widget, index) => (
+              <Box
+                className="col"
+                {...col}
+                key={`footer-widget-${index}`}
+              >
+                <Heading content={widget.title} {...titleStyle} />
+                <List>
+                  {widget.menuItems.map((item, i) => (
+                    <ListItem key={`footer-list-item-${i}`}>
+                      <Link href={item.url}>
+                        <a className="ListItem">{item.text}</a>
+                      </Link>
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            ))}
+          </Box>
         </Box>
-        {/* End of footer logo column */}
-        <Box {...colTwo}>
-          {FooterData.map((widget, index) => (
-            <Box className="col" {...col} key={`footer-widget-${index}`}>
-              <Heading content={widget.title} {...titleStyle} />
-              <List>
-                {widget.menuItems.map((item, i) => (
-                  <ListItem key={`footer-list-item-${i}`}>
-                    <Link href={item.url}>
-                      <a className="ListItem">{item.text}</a>
-                    </Link>
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-          ))}
-        </Box>
-        {/* End of footer List column */}
-      </Box>
-    </Container>
-  </FooterWrapper>
+      </Container>
+    </FooterWrapper>
+  </>
 );
 
 // Footer style props
